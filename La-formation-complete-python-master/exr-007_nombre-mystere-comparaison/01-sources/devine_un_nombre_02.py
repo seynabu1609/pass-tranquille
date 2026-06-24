770db58a12a163efe1c52fc05c6b14519c85d92a
MoktarYacin <moktaryacinabdillahi@gmail.com>
2026-06-24 16:45:43 +0000

    Premier commit

---
+++ La-formation-complete-python-master/exr-007_nombre-mystere-comparaison/01-sources/devine_un_nombre_02.py
@@ -0,0 +1,5 @@
+nombre_mystere = 7
+nombre_utilisateur = input("Quel est le nombre mystère ? ")
+
+# Afficher à l'aide d'une structure conditionnelle si le nombre entré par l'utilisateur est plus grand,
+# plus petit ou égal au nombre mystère.
